010ca8e4a5df8e18e80c52ffb8e24d109e8d8d28
WCartagenaASC <wesleycartagena23@gmail.com>
2023-09-27 22:39:39 -0400

    initial-routes-setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import Home from './pages/Home/Home.js'
+import Crucible from './pages/Crucible/Crucible.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <main>
+            <Home/>
+            <Crucible/>
+        </main>
+    );
 }
 
-export default App;
+export default App
---
+++ src/pages/Crucible/Crucible.js
@@ -0,0 +1,7 @@
+export const Crucible = () => {
+    return(
+        <h1>Welcome to the Guardians Central Crucible Page</h1>
+    );
+}
+
+export default Crucible
