1387dc9de4b7d994610616414afb475e262d5311
WCartagenaASC <wesleycartagena23@gmail.com>
2023-09-28 22:36:26 -0400

    Removed navlink import from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faSquareXTwitter,faDiscord } from '@fortawesome/free-brands-svg-icons'
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
-import { NavLink, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Footer =() => {
     return(
