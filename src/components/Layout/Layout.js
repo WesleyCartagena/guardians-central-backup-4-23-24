244bb8f46d62fb36219b2ae025241cd1ac723310
WCartagenaASC <wesleycartagena23@gmail.com>
2023-09-28 22:23:41 -0400

    footer-finished

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 const Layout = () => {
     return ( 
         <div className="App"> 
             <Outlet />
+            <Footer/>
         </div>
     )
 }
