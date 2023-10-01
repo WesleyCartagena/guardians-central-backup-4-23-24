7bcc637f781af59f1cac63f765b568d549bbfa2e
WCartagenaASC <wesleycartagena23@gmail.com>
2023-10-01 02:25:36 -0400

    replaced class attribute with className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,18 +7,18 @@ import { Link } from 'react-router-dom'
 const Footer =() => {
     return(
 
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 bg-dark fixed-bottom">
-            <div class="col-md-4 d-flex align-items-center">
-                <Link class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 text-light" to="/">
-                    <FontAwesomeIcon icon={faMoon} size="xl text-light px-2" />
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 bg-dark fixed-bottom">
+            <div className="col-md-4 d-flex align-items-center">
+                <Link className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 text-light" to="/">
+                    <FontAwesomeIcon className = "text-light px-2" icon={faMoon} size="xl" />
                 </Link>
-                <span class="mb-3 mb-md-0 text-body-secondary"><Link class="text-decoration-none link-light" to="/">© 2023 Guardians Central, Inc</Link></span>
+                <span className="mb-3 mb-md-0 text-body-secondary"><Link className="text-decoration-none link-light" to="/">© 2023 Guardians Central, Inc</Link></span>
             </div>
         
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
-                <li class="ms-3 text-light"><FontAwesomeIcon icon={faSquareXTwitter} size="xl" /></li>
-                <li class="ms-3 text-light"><FontAwesomeIcon icon={faGithub} size="xl" /></li>
-                <li class="ms-3 text-light"><FontAwesomeIcon icon={faDiscord} size="xl" /></li>
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
+                <li className="ms-3 text-light"><FontAwesomeIcon icon={faSquareXTwitter} size="xl" /></li>
+                <li className="ms-3 text-light"><FontAwesomeIcon icon={faGithub} size="xl" /></li>
+                <li className="ms-3 text-light"><FontAwesomeIcon icon={faDiscord} size="xl" /></li>
             </ul>
             </footer>
 
