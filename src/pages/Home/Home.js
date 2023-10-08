b1380a5e0cedd9363582510be46003e49442c1a7
WCartagenaASC <wesleycartagena23@gmail.com>
2023-10-08 12:15:02 -0400

    added background image to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,12 @@ import '../../App.scss'
 import TimerCard from '../../components/TimerCard/TimerCard';
 import Accordian from '../../components/Accordian/Accordian'
 import TwitterTabs from '../../components/TwitterTabs/TwitterTabs';
-// Make background image only be background for content or set that a default background for everythng a load anothe for content
-// Will be loading another for each page anyway
+import './Home.scss'
+
 const Home = () => {
 
     return(
-        <div className="Home">
+        <div className="home">
             <div className='d-inline-flex flex-column justify-content-center w-100'>
                 <TimerCard/>
                 <Accordian/>
