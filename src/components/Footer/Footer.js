2fbdd9999ffcc9c97574c6e9c922ad5fa81133c2
WCartagenaASC <wesleycartagena23@gmail.com>
2023-10-08 12:04:01 -0400

    fixed footer spacing issues on breakpoints

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,10 @@ const Footer =() => {
 
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 bg-dark relative-bottom">
             <div className="col-md-4 d-flex align-items-center">
-                <Link className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 text-light" to="/">
+                <Link className="mb-1 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1 text-light" to="/">
                     <FontAwesomeIcon className = "text-light px-2" icon={faMoon} size="xl" />
                 </Link>
-                <span className="mb-3 mb-md-0 text-body-secondary"><Link className="text-decoration-none link-light" to="/">© 2023 Guardians Central, Inc</Link></span>
+                <span className="mb-1 mb-md-0 text-body-secondary"><Link className="text-decoration-none link-light" to="/">© 2023 Guardians Central, Inc</Link></span>
             </div>
         
             <ul className="nav col-md-4 justify-content-end list-unstyled d-flex px-3">
