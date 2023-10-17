b5e554fb53844878687c6817f079fe4b7e5a0327
WCartagenaASC <wesleycartagena23@gmail.com>
2023-10-17 00:04:13 -0400

    testing-backend

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
 
-
 let db = new sqlite3.Database('world_sql_content_683f6ecb4976bed01885c945993321d8.sqlite3', (err) =>{
     if (err) {
         return console.error(err.message);
--- src/App.js
+++ src/App.js
@@ -9,19 +9,11 @@ import Layout from './components/ui/Layout'
 import './App.scss';
 
 function App () {
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        fetch("http://localhost:8000/message")
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
-    }, []);
-
     return (
         <>
            <Routes>
                 <Route path="/" element={<Layout />}>
-                    <Route index element={<Home serverMessage={message} />} />
+                    <Route index element={<Home/>} />
                     <Route path="crucible" element={ <Crucible/> } />
                     <Route path="vanguard" element={ <Vanguard/> } />
                     <Route path="gambit" element={ <Gambit/> } />
