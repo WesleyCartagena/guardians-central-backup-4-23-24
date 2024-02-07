9dbdeee075bf8043e46cf2314f3f1c937acbf058
WCartagenaASC <wesleycartagena23@gmail.com>
2024-02-07 17:37:35 -0500

    leftover files

--- database/obj/Debug/net8.0/database.AssemblyInfo.cs
+++ database/obj/Debug/net8.0/database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220f5498df4369ae088fb72cc71ab1a854c6409f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3716a36179096d60197159745dda1b274847cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
