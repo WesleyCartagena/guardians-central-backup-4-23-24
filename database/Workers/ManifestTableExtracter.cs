9eb6ec513e0370f71907c4144d7516a8ed942e46
Adriel <adrielmartinez48@gmail.com>
2024-02-19 21:24:28 -0500

    Added implementation for adding DestinyHistoricalStatsDefinition data

--- database/Workers/ManifestTableExtracter.cs
+++ database/Workers/ManifestTableExtracter.cs
@@ -16,40 +16,68 @@ public class ManifestTableExtracter{
                 // Get table names from sqlite_master
                 List<string> tableNames = GetSQLiteTableNames(sqliteConnection);
                 
-                foreach(string tableName in tableNames){
-                    Console.WriteLine(tableName);
-                }
-                // MySQL connection
+                // foreach(string tableName in tableNames){
+                //     Console.WriteLine(tableName);
+                // }
+                // Create MS SQL Server connection
                 string sqlConnectionString = $"Server={Server};Database={Database};TrustServerCertificate=True;Uid={UserId};Pwd={Password};";
                 Console.WriteLine(sqlConnectionString);
                 using (SqlConnection mysqlConnection = new SqlConnection(sqlConnectionString)){
                     try{
                         mysqlConnection.Open();
-                        
+                        // loop through all tables and their rows
                         foreach (string tableName in tableNames){
-                            // SQLite query
-                            using (SQLiteCommand sqliteCommand = new SQLiteCommand($"SELECT * FROM {tableName}", sqliteConnection)){
-                                using (SQLiteDataReader reader = sqliteCommand.ExecuteReader()){
-                                    // Read and process data
-                                    while (reader.Read()){
-                                        // Assuming 'column1' and 'column2' are column names in SQLite
-                                        int Id = Convert.ToInt32(reader["Id"]);
-                                        byte[] jsonBytes = (byte[])reader["Json"];
-                                        string jsonString = Encoding.UTF8.GetString(jsonBytes);
-                                        
-                                        if(TableExists(connection:mysqlConnection, tableName:tableName) == false){
-                                            using (SqlCommand createTableCommand = new SqlCommand($"CREATE TABLE {tableName} (Id INT, Json VARCHAR(MAX))", mysqlConnection)) {
-                                                createTableCommand.ExecuteNonQuery();
+                            // DestinyHistoricalStatsDefinition 
+                            if (tableName == "DestinyHistoricalStatsDefinition"){
+                                using (SQLiteCommand sqliteCommand = new SQLiteCommand($"SELECT * FROM {tableName}", sqliteConnection)){
+                                    using (SQLiteDataReader reader = sqliteCommand.ExecuteReader()){
+                                        while (reader.Read()){
+                                            string key = (string)reader["key"];
+                                            byte[] jsonBytes = (byte[])reader["Json"];
+                                            string jsonString = Encoding.UTF8.GetString(jsonBytes);
+
+                                            if (TableExists(connection: mysqlConnection, tableName: tableName) == false){
+                                                using (SqlCommand createTableCommand = new SqlCommand($"CREATE TABLE {tableName} ([key] VARCHAR(MAX), Json VARCHAR(MAX))", mysqlConnection)){
+                                                    createTableCommand.ExecuteNonQuery();
+                                                }
+                                            }
+                                            
+                                            // MySQL query
+                                            using (SqlCommand sqlCommand = new SqlCommand($"INSERT INTO {tableName} ([key], Json) VALUES (@key, @Json)", mysqlConnection)){
+                                                // Set parameter values and execute query
+                                                sqlCommand.Parameters.AddWithValue("@key", key);
+                                                sqlCommand.Parameters.AddWithValue("@Json", jsonBytes);
+                                                sqlCommand.ExecuteNonQuery();
                                             }
                                         }
+                                    }
+                                }
 
+                            }
+                            else{
+                                // SQLite query
+                                using (SQLiteCommand sqliteCommand = new SQLiteCommand($"SELECT * FROM {tableName}", sqliteConnection)){
+                                    using (SQLiteDataReader reader = sqliteCommand.ExecuteReader()){
+                                        // Read and process data
+                                        while (reader.Read()){
+                                            // Assuming 'column1' and 'column2' are column names in SQLite
+                                            int Id = Convert.ToInt32(reader["Id"]);
+                                            byte[] jsonBytes = (byte[])reader["Json"];
+                                            string jsonString = Encoding.UTF8.GetString(jsonBytes);
+                                            
+                                            if(TableExists(connection:mysqlConnection, tableName:tableName) == false){
+                                                using (SqlCommand createTableCommand = new SqlCommand($"CREATE TABLE {tableName} (Id INT, Json VARCHAR(MAX))", mysqlConnection)) {
+                                                    createTableCommand.ExecuteNonQuery();
+                                                }
+                                            }
 
-                                        // MySQL query
-                                        using (SqlCommand sqlCommand = new SqlCommand($"INSERT INTO {tableName} (Id, Json) VALUES (@Id, @Json)", mysqlConnection)){
-                                            // Set parameter values and execute query
-                                            sqlCommand.Parameters.AddWithValue("@Id", Id);
-                                            sqlCommand.Parameters.AddWithValue("@Json", jsonBytes);
-                                            sqlCommand.ExecuteNonQuery();
+                                            // MySQL query
+                                            using (SqlCommand sqlCommand = new SqlCommand($"INSERT INTO {tableName} (Id, Json) VALUES (@Id, @Json)", mysqlConnection)){
+                                                // Set parameter values and execute query
+                                                sqlCommand.Parameters.AddWithValue("@Id", Id);
+                                                sqlCommand.Parameters.AddWithValue("@Json", jsonBytes);
+                                                sqlCommand.ExecuteNonQuery();
+                                            }
                                         }
                                     }
                                 }
@@ -75,7 +103,7 @@ public class ManifestTableExtracter{
         using (SQLiteCommand command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table'", connection)){
             using (SQLiteDataReader reader = command.ExecuteReader()){
                 while (reader.Read()){
-                    string tableName = reader["name"].ToString();
+                    string tableName = (string)reader["name"];
                     tableNames.Add(tableName);
                 }
             }
